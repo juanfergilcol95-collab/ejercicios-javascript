c96427c9033c3ce0088ada23a481a240096f4ab3
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:30:54 -0500

    Agrega ejercicio de operadores y cálculos

---
+++ 04-operadores.js
@@ -0,0 +1,14 @@
+// Ejercicio: operadores y cálculos en JavaScript
+
+let precio = 25000;
+let cantidad = 3;
+
+let subtotal = precio * cantidad;
+let descuento = subtotal * 0.10;
+let total = subtotal - descuento;
+
+console.log("Precio del producto:", precio);
+console.log("Cantidad:", cantidad);
+console.log("Subtotal:", subtotal);
+console.log("Descuento:", descuento);
+console.log("Total a pagar:", total);
