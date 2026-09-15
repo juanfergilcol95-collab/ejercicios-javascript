ffe9fd857c0dddc57c16e8e5a81e21aa3f165e4c
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:29:43 -0500

    Agrega ejercicio de tipos de datos

---
+++ 03-tipos-datos.js
@@ -0,0 +1,13 @@
+// Ejercicio: tipos de datos en JavaScript
+
+let nombre = "Juan Fernando"; // String
+let edad = 31; // Number
+let estudiante = true; // Boolean
+let ciudad; // Undefined
+let telefono = null; // Null
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Es estudiante?:", estudiante);
+console.log("Ciudad:", ciudad);
+console.log("Teléfono:", telefono);
