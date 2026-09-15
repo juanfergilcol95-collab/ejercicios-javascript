17970d9002d4b9c26e005e436682b7879a8672ad
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:28:55 -0500

    Agrega ejercicio de variables

---
+++ 02-variables.js
@@ -0,0 +1,9 @@
+// Ejercicio: uso de variables
+
+let nombre = "Juan Fernando";
+let edad = 31;
+let ciudad = "Medellín";
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
