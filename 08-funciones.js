3ea9d20eb85aa42f8ee2ababd6ecadf8197251c1
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:37:15 -0500

    Agrega ejercicio de funciones

---
+++ 08-funciones.js
@@ -0,0 +1,9 @@
+// Ejercicio: funciones en JavaScript
+
+function saludar(nombre) {
+    return "Hola, " + nombre + ". Bienvenido a JavaScript.";
+}
+
+let mensaje = saludar("Juan Fernando");
+
+console.log(mensaje);
