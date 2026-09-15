1da02c0a19766c555d7669507e4cfaec818ad5f3
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:38:28 -0500

    Agrega ejercicio de arrays

---
+++ 10-arrays.js
@@ -0,0 +1,11 @@
+// Ejercicio: arrays en JavaScript
+
+let productos = ["Paracetamol", "Ibuprofeno", "Vitamina C", "Acetaminofén"];
+
+console.log("Lista de productos:");
+console.log(productos);
+
+console.log("Primer producto:", productos[0]);
+console.log("Segundo producto:", productos[1]);
+
+console.log("Cantidad de productos:", productos.length);
