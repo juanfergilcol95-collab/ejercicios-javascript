ca7b8076fe418f178e868fe77dec8934da60fea0
juanfernandogil <juanfergilcol95@gmail.com>
2026-09-15 10:36:37 -0500

    Agrega ejercicio de clasificación por edad

---
+++ 07-rango-edad.js
@@ -0,0 +1,13 @@
+// Ejercicio: clasificación según la edad
+
+let edad = 25;
+
+if (edad < 13) {
+    console.log("Eres un niño.");
+} else if (edad < 18) {
+    console.log("Eres un adolescente.");
+} else if (edad < 60) {
+    console.log("Eres un adulto.");
+} else {
+    console.log("Eres un adulto mayor.");
+}
